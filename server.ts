e9ac18198e3ab188e0491bf03f93c70d954824ce
kamzzi <pluzeq@gmail.com>
2024-04-25 16:24:00 +0200

    feat: dotenv
    Add dotenv with PORT variable, setup in server.

--- server.ts
+++ server.ts
@@ -1,13 +1,16 @@
 import { startServer } from ".";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./config/.env" });
 
 const fastify = startServer();
 
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 const init = async () => {
   try {
     await fastify.listen({ port: PORT });
-    console.log(`Server successfully running!`);
+    console.log(`Server successfully running at port ${PORT}!`);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
