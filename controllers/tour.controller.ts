4d67d2a0345eea6bc356466ce64800d7ea6490d3
kamzzi <pluzeq@gmail.com>
2024-04-25 16:35:26 +0200

    feat: prisma
    Add prisma and configuration with fastify.

--- controllers/tour.controller.ts
+++ controllers/tour.controller.ts
@@ -1,11 +1,18 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const getTours = async (
   request: FastifyRequest,
   reply: FastifyReply
 ) => {
+  const tours = await prisma.tour.findMany();
+
   reply.status(200).send({
     message: "status",
-    data: [],
+    data: {
+      tours,
+    },
   });
 };
